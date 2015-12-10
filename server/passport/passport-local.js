a4965c31c0cd8b623bc73d6e856a444314820f57
Siyuan Gao <siyuan@eventbase.com>
2015-12-10 14:00:46 -0800

    Bring back the dragons

---
+++ server/passport/passport-local.js
@@ -0,0 +1,62 @@
+/*eslint-env node */
+/*globals err */
+var LocalStrategy   = require('passport-local').Strategy;
+var User = require('../mongo_modules/user.js');
+
+module.exports = function(passport) {
+  passport.serializeUser(function(user,done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(id, user) {
+      done(err, user);
+    });
+  });
+  passport.use('local-login', new LocalStrategy({
+    usernameField : 'username',
+    passwordField : 'password'
+  },
+  function(username,password,done) {
+    User.findOne({'local.username' : username}, function(err,user) {
+      if(err){
+        return done(err);
+      }
+      if(!user){
+        return done(null, false, {message : 'no such user'});
+      }
+
+      if(!user.validPassword(password)){
+        return done(null, false, {message : 'wrong password'});
+      }
+
+      return done(null, user);
+    });
+  }));
+
+  passport.use('local-signup', new LocalStrategy({
+    usernameField : 'username',
+    passwordField : 'password'
+  },
+  function(username,password,done) {
+    User.findOne({'local.username' : username}, function(err, user) {
+      if(err){
+        return done(err);
+      }
+
+      if(user){
+        return done(null, false, {message : 'used user'});
+      } else {
+        var newUser = new User();
+        newUser.local.username = username;
+        newUser.local.password = newUser.generateHash(password);
+
+        newUser.save(function(err) {
+          if(err)
+            throw err;
+          return done(null, newUser);
+        });
+      }
+    });
+  }));
+}
