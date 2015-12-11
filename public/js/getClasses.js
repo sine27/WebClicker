3a1a2284bbd4a3b46a3ba3dce626256f0059b672
sine27 <sinefeng@126.com>
2015-12-11 07:13:48 -0500

    able to load class

--- public/js/getClasses.js
+++ public/js/getClasses.js
@@ -3,36 +3,25 @@ var classes = [];
 function getclasses(){
     var url = "/class";
     $.get(url).done(function (data) {
-        console.log(JSON.stringify(data));
-        for (var i = 0; i < 4; i++) {
-            classes.push(data.Classes.pop());
+        //alert(JSON.stringify(data));
+        var length = data.Classes.length;
+        for (var i = 1; i <= length; i++) {
+            var class_i = data.Classes.pop();
+
+            var classid = "class" + i + "name";
+            document.getElementById(classid).innerHTML = class_i.title;
+
+            var detailid = "class" + i + "detail";
+            document.getElementById(detailid).innerHTML = class_i.detail;
+
+console.log(classid);
+
         };
-        document.getElementById('classlist').appendChild(makeUL(classes));
 
     }).fail(function(){
     	alert("Wrong Username or Password !");
     });
 }
-
-function makeUL(array) {
-    // Create the list element:
-    var list = document.createElement('ul');
-
-    for(var i = 0; i < array.length; i++) {
-        // Create the list item:
-        var item = document.createElement('li');
-
-        // Set its contents:
-        item.appendChild(document.createTextNode(array[i]));
-
-        // Add it to the list:
-        list.appendChild(item);
-    }
-
-    // Finally, return the constructed list:
-    return list;
-}
-
 
 getclasses();
 // Add the contents of options[0] to #foo:
